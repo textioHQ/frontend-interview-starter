fe7e18585f532f6d1dac19829bc72a2dfa12d2d4
Adam Sanderson <adams@textio.com>
2024-10-17 23:32:01 -0700

    Replace CreateReactApp with Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import { text, highlights } from '@textio/frontend-interview-data';
-import logo from './logo.svg';
+import logo from './assets/react.svg'
 import './App.css';
 
 console.log("Textio string:", text);
@@ -11,19 +10,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <main>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
+      </main>
+    </div >
   );
 }
 
